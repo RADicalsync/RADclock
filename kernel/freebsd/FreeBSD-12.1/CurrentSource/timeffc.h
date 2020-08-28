f687591582a84654b1a94b5347ef7e6fcad58b89
Darryl Veitch <Darryl.Veitch@uts.edu.au>
2020-08-28 17:08:28 +1000

    Final seeding of FreeBSD 12.1 directory with Original 12.1 files.

--- kernel/freebsd/FreeBSD-12.1/CurrentSource/timeffc.h
+++ kernel/freebsd/FreeBSD-12.1/CurrentSource/timeffc.h
@@ -28,7 +28,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- * $FreeBSD: releng/12.0/sys/sys/timeffc.h 326256 2017-11-27 15:01:59Z pfg $
+ * $FreeBSD: releng/12.1/sys/sys/timeffc.h 326256 2017-11-27 15:01:59Z pfg $
  */
 
 #ifndef _SYS_TIMEFF_H_
